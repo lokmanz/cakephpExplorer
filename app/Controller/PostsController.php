926d51b6ec3c7c698544a700baca659f0ebc0b04
lokmanz <avgbf66@gmail.com>
2014-06-20 11:11:20 +0800

    Done view a post according to post id with comment

--- app/Controller/PostsController.php
+++ app/Controller/PostsController.php
@@ -3,6 +3,8 @@
 class PostsController extends AppController {
     public $helpers = array('Html', 'Form');
 
+    //http://localhost/cakephpExplorer/posts/index
+    //list all posts
     public function index() {
     	//sets the view variable called ‘posts’ equal to the return value of the find('all') 
     	///method of the Post model. 
@@ -10,6 +12,22 @@ class PostsController extends AppController {
     	//CakePHP’s naming conventions.
         $this->set('posts', $this->Post->find('all'));
     }
+
+    ////http://localhost/cakephpExplorer/posts/index/view
+    //view a post according to post id
+    public function view($id = null) {
+        if (!$id) {
+            throw new NotFoundException(__('Invalid post'));
+        }
+
+        $post = $this->Post->findById($id);
+        if (!$post) {
+            throw new NotFoundException(__('Invalid post'));
+        }
+      
+        $this->set('post', $post);
+    }
+
 }
 
 ?>
